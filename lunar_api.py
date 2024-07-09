b03032e0894820f195418ac3faa0e9481afa40ac
Path byte <62700063+pgalari@users.noreply.github.com>
2024-07-09 20:22:44 +0100

    Update lunar_api.py

--- lunar_api.py
+++ lunar_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request
 from datetime import datetime
 import ephem
 from astral import moon, LocationInfo
